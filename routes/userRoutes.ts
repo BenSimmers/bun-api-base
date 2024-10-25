97373c995b24dddaae0f47fd95da062c40dc31fe
Benjamin Simmers <bej1380@gmail.com>
2024-10-25 23:48:40 +1000

    feat: add types and some sort of DI approach

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -4,29 +4,21 @@ import type { User } from "../services/Users/model";
 
 const router = Router();
 
-// when doing a real project, this should be replaced with a database
-const users: User[] = [
-  { id: 1, name: "John Doe", age: 25 },
-  { id: 2, name: "Jane Doe", age: 24 },
-  { id: 3, name: "Jim Doe", age: 23 },
-  { id: 4, name: "Jill Doe", age: 22 },
-  { id: 5, name: "Jack Doe", age: 21 },
-];
 
 router.get("/", async (req: Request, res: Response) => {
-  const userList = userService.getUsers(users);
+  const userList = userService.getUsers();
   return res.status(200).json(userList);
 });
 
 router.post("/", async (req: Request, res: Response) => {
   const { name, age } = req.body;
-  const newUser = userService.createUser(users, name, age);
+  const newUser = userService.createUser(name, age);
   return res.status(201).json(newUser);
 });
 
 router.get("/:id", async (req: Request, res: Response) => {
   const { id } = req.params;
-  const user = userService.getUserById(users, Number(id));
+  const user = userService.getUserById(Number(id));
   if (!user) {
     return res.status(404).json({ message: "User not found" });
   }
--- services/Users/users.ts
+++ services/Users/users.ts
@@ -1,23 +1,27 @@
 import type { User } from "./model";
 
-export function getUsers(currentUsers: User[]): User[] {
-  return currentUsers;
+// This is a mock collection of users
+// In a real application, this would be replaced by a database
+// and our services would interact with the database
+const users: User[] = [
+  { id: 1, name: "John Doe", age: 25 },
+  { id: 2, name: "Jane Doe", age: 24 },
+  { id: 3, name: "Jim Doe", age: 23 },
+  { id: 4, name: "Jill Doe", age: 22 },
+  { id: 5, name: "Jack Doe", age: 21 },
+];
+
+export function getUsers(): User[] {
+  return users;
 }
 
-export function getUserById(
-  currentUsers: User[],
-  id: number
-): User | undefined {
-  return currentUsers.find((user) => user.id === id);
+export function getUserById(id: number): User | undefined {
+  return users.find((user) => user.id === id);
 }
 
-export function createUser(
-  currentUsers: User[],
-  name: string,
-  age: number
-): User {
-  const id = currentUsers.length + 1;
+export function createUser(name: string, age: number): User {
+  const id = users[users.length - 1].id + 1;
   const newUser = { id, name, age };
-  currentUsers.push(newUser);
+  users.push(newUser);
   return newUser;
 }
